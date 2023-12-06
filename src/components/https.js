82bca8bf203509af7ccb80dcdca73c187878299a
Olugbenga Ajewole <123884021+ajewoleOlugbenga@users.noreply.github.com>
2023-12-06 18:25:18 +0100

    using async function

---
+++ src/components/https.js
@@ -0,0 +1,32 @@
+export async function fetchingAvailablePlaces() {
+  const response = await fetch("http://localhost:3000/places");
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error("404 - not found");
+  }
+  return resData.places;
+}
+
+export async function fetchingUserPlaces() {
+  const response = await fetch("http://localhost:3000/user-places");
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error("404 - not found");
+  }
+  return resData.places;
+}
+
+export async function updatedUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places: places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const resData = await response.json();
+  if (!response.ok) {
+    throw new Error("Failed to update user data.");
+  }
+  return resData.message;
+}
